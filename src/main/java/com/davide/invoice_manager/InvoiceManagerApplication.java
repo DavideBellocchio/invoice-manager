22a495120663fc16c198aa25832b6ae6d745ac58
Davide <davide.bellocchio.db@gmail.com>
2026-06-25 18:02:38 +0200

    chore: configure jpa auditing and application settingsy

--- src/main/java/com/davide/invoice_manager/InvoiceManagerApplication.java
+++ src/main/java/com/davide/invoice_manager/InvoiceManagerApplication.java
@@ -2,8 +2,10 @@ package com.davide.invoice_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class InvoiceManagerApplication {
 
 	public static void main(String[] args) {
